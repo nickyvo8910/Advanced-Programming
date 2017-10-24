e60029585243ed577187c1d419be964c4db35b5a
Nhut Vo <2253715v@sibu.(none)>
2017-10-24 22:35:46 +0100

    none

--- date.c
+++ date.c
@@ -61,14 +61,7 @@ int date_compare(Date *date1, Date *date2){
 /*
  * date_destroy returns any storage associated with `d' to the system
  */
-void date_destroy(Date *d){
-	//d = (Date *) realloc(d, sizeof(Date)-(sizeof(__int64)));
-	free(d->day);
-	free(d->month);
-	free(d->year);
-	free(d);
-}
-
+void date_destroy(Date *d){}
 void to_string(Date *d){
 	printf("Day : %d, Month : %d, Year : %d \n",d->day,d->month,d->year);
 }
